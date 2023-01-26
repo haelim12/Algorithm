d8761e1a2bc257e109fc24d40d7a7f04ff0fc8fd
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-01-26 11:11:45 +0900

    [Silver IV] Title: 제로, Time: 3836 ms, Memory: 31240 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,15 +1,12 @@
 K = int(input())
 
-number_list = []
 result = []
 for j in range(K):
     number = int(input())
-    number_list.append(number)
 
-for i in range(len(number_list)):
-    if number_list[i] != 0:
-        result.append(number_list[i])
-    elif number_list[i] == 0:
+    if number != 0:
+        result.append(number)
+    elif number == 0:
         result.pop()
 
 print(sum(result))
