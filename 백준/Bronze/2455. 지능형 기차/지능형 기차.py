466e229a81eb366cbfc36a2fcbaa6090ca425c24
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-02-06 14:00:49 +0900

    [Bronze III] Title: 지능형 기차, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2455. 지능형 기차/지능형 기차.py
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.py
@@ -1,22 +1,9 @@
-# 지능형 기차
-
 train = 0
 train_list = []
 
-out_1, in_1 = map(int, input().split())
-train = in_1
-train_list.append(train)
-
-out_2, in_2 = map(int, input().split())
-train = train - out_2 + in_2
-train_list.append(train)
-
-out_3, in_3 = map(int, input().split())
-train = train - out_3 + in_3
-train_list.append(train)
-
-out_4, in_4 = map(int, input().split())
-train = train - out_4
-train_list.append(train)
+for i in range(4):
+    t_out, t_in = map(int, input().split())
+    train -= t_out - t_in
+    train_list.append(train)
 
 print(max(train_list))
