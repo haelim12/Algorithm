c2c4423d9f4288468c395e469470746349468ed1
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-08-13 23:22:38 +0900

    [D3] Title: 오목 판정, Time: 168 ms, Memory: 23,008 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -1,83 +1,81 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for (int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
-			sc.nextLine();
-			Character[][] arr = new Character[N][N];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-			for (int i = 0; i < arr.length; i++) {
-				String str = sc.nextLine();
-				for (int j = 0; j < arr.length; j++) {
-					arr[i][j] = str.charAt(j);
-				}
-			}
-			
-//			for (int i = 0; i < arr.length; i++) {
-//				System.out.println(Arrays.toString(arr[i]));
-//			}
-			
-			boolean omok = false;
-			
-			// 'o' 찾기
-			for (int i = 0; i < arr.length; i++) {
-				for (int j = 0; j < arr.length; j++) {
-					if (arr[i][j] == 'o') {
-						// 우하향 대각선 탐색
-						int count = 0;
-						for (int d = 0; d < 5; d++) {
-							if (i+d<N && j+d<N && arr[i+d][j+d]=='o') {
-								count++;
-							}
-							if (count == 5)
-								omok = true;
-						}
-						
-						// 좌하향 대각선 
-						count = 0;
-						for (int d = 0; d < 5; d++) {
-							if (i+d<N && j-d>=0 && arr[i+d][j-d]=='o') {
-								count++;
-							}
-							if (count == 5)
-								omok = true;
-						}
-						
-						// 가로 탐색
-						count = 0;
-						for (int d = 0; d < 5; d++) {
-							if (j+d<N && arr[i][j+d]=='o') {
-								count++;
-							}
-							if (count == 5)
-								omok = true;
-						}
-						
-						// 세로 탐색
-						count = 0;
-						for (int d = 0; d < 5; d++) {
-							if (i+d<N && arr[i+d][j]=='o') {
-								count++;
-							}
-							if (count == 5)
-								omok = true;
-						}
-					}
-				}
-			}
-			
-			if (omok) {
-				System.out.printf("#%d YES\n", tc);
-			} else {
-				System.out.printf("#%d NO\n", tc);
-			}
-		}
-		
-	}
+        int T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = sc.nextInt();
+            sc.nextLine();
+            Character[][] arr = new Character[N][N];
+
+            for (int i = 0; i < arr.length; i++) {
+                String str = sc.nextLine();
+                for (int j = 0; j < arr.length; j++) {
+                    arr[i][j] = str.charAt(j);
+                }
+            }
+
+            boolean omok = false;
+            int count = 0; // count가 5가 되면 오목
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[i][j]=='o') {
+                        // 우하향 대각선 탐색
+                        count=0;
+                        for (int d = 0; d < 5; d++) {
+                            if (i+d<N && j+d<N && arr[i+d][j+d]=='o') {
+                                count++;
+                            }
+                        }
+                        if (count==5) {
+                            omok = true;
+                        }
+
+                        // 좌하향 대각선 탐색
+                        count=0;
+                        for (int d = 0; d < 5; d++) {
+                            if (i+d<N && j-d>=0 && arr[i+d][j-d]=='o') {
+                                count++;
+                            }
+                        }
+                        if (count==5) {
+                            omok = true;
+                        }
+
+                        // 가로 탐색
+                        count=0;
+                        for (int d = 0; d < 5; d++) {
+                            if (j+d<N && arr[i][j+d]=='o') {
+                                count++;
+                            }
+                        }
+                        if (count==5) {
+                            omok = true;
+                        }
+
+                        // 세로 탐색
+                        count=0;
+                        for (int d = 0; d < 5; d++) {
+                            if (i+d<N && arr[i+d][j]=='o') {
+                                count++;
+                            }
+                        }
+                        if (count==5) {
+                            omok = true;
+                        }
+                    }
+                }
+            }
+
+            if (omok) {
+                System.out.printf("#%d YES\n", tc);
+            } else {
+                System.out.printf("#%d NO\n", tc);
+            }
+        }
+    }
 }
