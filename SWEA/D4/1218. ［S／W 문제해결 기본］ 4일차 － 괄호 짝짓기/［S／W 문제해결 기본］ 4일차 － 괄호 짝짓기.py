f9eecb69b6af52faf3b17f529f37747309ee1059
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-08-16 15:01:43 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 173 ms, Memory: 58,272 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
@@ -1,39 +1,61 @@
-for j in range(1, 11):
-    N = int(input())
-    case = list(input())
-    check = []
-    result = 1
+import java.util.Scanner;
+import java.util.Stack;
 
-    for i in range(N):
-        if case[i] == ')':
-            if '(' in check:
-                check.pop(check.index('('))
-            else:
-                result = 0
-                break
-        
-        elif case[i] == ']':
-            if '[' in check:
-                check.pop(check.index('['))
-            else : 
-                result = 0
-                break        
-        
-        elif case[i] == '}':
-            if "{" in check:
-                check.pop(check.index('{'))
-            else : 
-                result = 0
-                break
-
-        elif case[i] == '>':
-            if '<' in check:
-                check.pop(check.index('<'))
-            else : 
-                result = 0
-                break
-
-        else:
-            check.append(case[i])
-
-    print(f'#{j} {result}')
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int tc = 1; tc <= 10; tc++) {
+			int N = sc.nextInt();
+			Stack<Character> stack = new Stack<>();
+			boolean check = true;
+			sc.nextLine();
+			String str = sc.nextLine();
+			label : for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) == '(') {
+//					System.out.println(str.charAt(i));
+					stack.push('(');
+					
+				} else if (str.charAt(i) == ')') {
+					if (stack.peek() == '(') {
+						stack.pop();
+					} else {
+						check = false;
+					}
+				} else if (str.charAt(i) == '[') {
+					stack.push('[');
+				} else if (str.charAt(i) == ']') {
+					if (stack.peek() == '[') {
+						stack.pop();
+					} else {
+						check = false;
+					}
+				} else if (str.charAt(i) == '{'){
+					stack.push('{');
+				} else if (str.charAt(i) == '}') {
+					if (stack.peek() == '{') {
+						stack.pop();
+					} else {
+						check = false;
+					}
+				} else if (str.charAt(i) == '<') {
+					stack.push('<');
+				} else {
+					if (stack.peek() == '<') {
+						stack.pop();
+					} else {
+						check = false;
+					}
+				}
+			}
+			
+			if (stack.isEmpty() && check) {
+				System.out.printf("#%d 1\n", tc);
+			} else {
+				System.out.printf("#%d 0\n", tc);
+			}
+		}
+		
+		
+	}
+}
