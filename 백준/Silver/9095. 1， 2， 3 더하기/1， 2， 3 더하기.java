d16304ecdcd576961faa5640f4aa112a846bfe38
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-08-30 22:47:18 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 116 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        dp = new int[11];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+        }
+
+        for (int tc = 1; tc <= T ; tc++) {
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
+        }
+    }
+}
