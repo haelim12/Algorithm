0552ba21ee455160dc6178e7bf752571412b8a3e
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-09-02 02:33:45 +0900

    [Silver III] Title: N과 M (8), Time: 160 ms, Memory: 19056 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    static StringBuilder sb;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        sb = new StringBuilder();
+        int[] newArr = new int[M];
+        recursion(0, newArr, 0);
+
+        System.out.println(sb);
+
+    }
+    public static void recursion(int index, int[] newArr, int start) {
+        if (index == M) {
+            for (int i = 0; i < newArr.length; i++) {
+                sb.append(newArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            newArr[index] = arr[i];
+            recursion(index+1, newArr, i);
+        }
+    }
+}
