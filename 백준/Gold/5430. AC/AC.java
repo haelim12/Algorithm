9aba3d379a2d1b2c9de523843377648795c8ab76
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-09-25 15:35:16 +0900

    [Gold V] Title: AC, Time: 2616 ms, Memory: 109752 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,104 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        main : for (int t = 0; t < T; t++) {
+			String met = br.readLine();
+			char[] mArr = met.toCharArray();
+			
+			
+			int N = Integer.parseInt(br.readLine());
+			String str = br.readLine();
+			str = str.replace("[", "");
+			str = str.replace("]", "");
+			String[] strArr = str.split(",");
 
-        int T = sc.nextInt();
-
-        for (int tc = 1; tc <= T; tc++) {
-            String str = sc.next();
-
-            char[]arr = new char[str.length()];
-
-            for (int i = 0; i < str.length(); i++) {
-                arr[i] = str.charAt(i);
-            }
-
-            int n = sc.nextInt();
-            sc.nextLine();
-            
-            String temp1 = sc.nextLine();
-            String temp2 = temp1.substring(1,temp1.length()-1);
-            String[] arr2 = temp2.split(",");
-            
-            int[] nums = new int[n];
-            int index = 0;
-            for (int i = 0; i < arr2.length; i++) {
-                if(!arr2[i].equals("")){
-                    nums[index] = Integer.parseInt(arr2[i]);
-                    index++;
-                }
-            }
-            
-            // StringTokenizer
-//            String s = sc.nextLine();
-//            if (n == 0) {
-//				System.out.println("error");
-//				break;
-//			}
-//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//            StringTokenizer st = new StringTokenizer(br.readLine(), "[,]");
-//            
-            Deque<Integer> deque = new ArrayDeque<>();
-            
-            for (int i = 0; i < nums.length; i++) {
-				deque.addLast(nums[i]);
+			List<Integer> list = new ArrayList<>();
+			for (int i = 0; i < strArr.length; i++) {
+				if(N > 0) list.add(Integer.parseInt(strArr[i]));
 			}
-
-            boolean reverse = false;
-            boolean empty = false;
-            
-            label : for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == 'R') { // R일 때
-                    reverse = !reverse;
-//                	System.out.println(deque);
-//                } else if (arr[i] == 'D') {
-                } else { // D일 때
-                    if (deque.isEmpty()) {
-                    	empty = true;
-//                    	System.out.println(deque);
-                        System.out.println("error");
-                        break label;
-                    } else {
-                    	if (reverse == false) {
-							deque.removeFirst();
-						} else {
-							deque.removeLast();
-						}
-                    }
-                }
-            }
-            StringBuilder sb = new StringBuilder();
-            sb.append("[");
-            if (!reverse && !empty) {
-				int temp = deque.size();
-				for (int i = 0; i < temp; i++) {
-					sb.append(deque.pollFirst());
-					if (i != temp-1) {
-						sb.append(",");
+			
+			int j = 0;
+			for (int i = 0; i < mArr.length; i++) {
+				if(mArr[i] == 'R') {
+					if(j == 0) {
+						j = list.size() - 1;
+					} else {
+						j = 0;
 					}
-				}
-			}else if(!empty && reverse){
-				int temp = deque.size();
-				for (int i = 0; i < temp; i++) {
-					sb.append(deque.pollLast());
-					if (i != temp-1) {
-						sb.append(",");
+				} else {
+					if(!list.isEmpty()) {
+						int x = list.remove(j);
+						if(j != 0) j = list.size()-1;
+					} else {
+						sb.append("error").append('\n');
+						continue main;
 					}
 				}
 			}
-            sb.append("]");
-            if(!empty)
-            	System.out.println(sb);
-        }
+			
+			sb.append("[");
+			
+			if(list.isEmpty()) {
+				sb.append(" ");
+			} else if(j > 0) {
+				for (; j >= 0; j--) {
+					sb.append(list.get(j)).append(",");
+				}
+			} else {
+				for (; j < list.size(); j++) {
+					sb.append(list.get(j)).append(",");
+				}
+			}
+			
+			sb.deleteCharAt(sb.length()-1).append("]").append('\n');
+			
+		}
+        sb.deleteCharAt(sb.length()-1);
+        System.out.print(sb);
     }
 }
