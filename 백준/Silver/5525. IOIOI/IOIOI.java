9e0bb2d8fbd00881206406447ac51f25e994a964
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-09-25 16:55:49 +0900

    [Silver I] Title: IOIOI, Time: 92 ms, Memory: 16020 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,34 +1,45 @@
-import java.beans.PropertyEditorSupport;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    
+	static int N, strN, cnt;
+	static StringBuilder sb = new StringBuilder();
+	static String str, str2;
+	
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        char[] word = br.readLine().toCharArray();
-
-        int count = 0;
-        int pattern = 0;
-
-        for (int i = 1; i < M-1; i++) {
-            if (word[i-1]=='I' && word[i]=='O' && word[i+1]=='I') {
-                pattern++;
-                if (pattern == N) {
-                    count++;
-                    pattern--;
-                }
-                i++;
-            } else{
-                pattern = 0;
-            }
-        }
-        System.out.println(count);
+        StringTokenizer st;
+        
+        N = Integer.parseInt(br.readLine());
+        strN = 2*N + 1;
+        sb.append("I");
+        m(0);
+        
+        str = sb.toString();
+        int len = Integer.parseInt(br.readLine());
+        
+        str2 = br.readLine();
+        
+        for (int i = 0; i < str2.length()-strN+1; i++) {
+			m2(i);
+		}
+        
+        System.out.println(cnt);
     }
+    
+    static void m(int cnt) {
+    	if(cnt == N) return;
+    	sb.append("OI");
+    	m(cnt+1);
+    }
+    
+    static void m2(int idx) {
+    	String temp = str2.substring(idx, idx+strN);
+    	if(temp.equals(str)) cnt++;
+    }
+    
+    
+    
 }
