e746f6b0c7e8bfa83d54b19d326a22740534ca8a
haelim12 <121421005+haelim12@users.noreply.github.com>
2023-10-17 23:05:30 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 112 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int g = gcd(a,b);
+        int l = (a*b) / g;
+
+        System.out.println(g);
+        System.out.println(l);
+    }
+    
+    public static int gcd(int a, int b) {
+        if (b==0) {
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+}
